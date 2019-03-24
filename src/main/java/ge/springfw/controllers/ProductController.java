e94c45bbf520f44eec9a27c654bc51a1b18f12b2
Kranthi <achantakranthi@gmail.com>
2019-03-24 11:42:42 +0530

    with docker file

--- src/main/java/ge/springfw/controllers/ProductController.java
+++ src/main/java/ge/springfw/controllers/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,7 +40,7 @@ public class ProductController {
     }
     
     @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Product createProduct(@PathVariable Product product) {
+    public Product createProduct(@RequestBody Product product) {
     		return productService.saveOrUpdate(product);
     }
 
